e4d46af0acac88213936cc0b99ed2996cae0e08a
okavsar9-boop <okavsar9@gmail.com>
2026-03-09 05:08:14 +0100

    Just create the post function to my FastAPI

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 from fastapi import FastAPI, Path
+from typing import Optional
+from pydantic import BaseModel
+
+class Item(BaseModel):
+    product: str
+    price: float
+    brand: Optional[str] = None
+    quantity: Optional[int] = None
 
 app = FastAPI()
 
@@ -26,15 +34,24 @@ def home():
     return {"Data" : "Welcome to the page"}
 
 @app.get("/store/{item_id}")
-def store(item_id : int = Path( description= "The ID of the item you want to get ",gt= 0, lt = 3)):
+def store(item_id : int = Path( description= "The ID of the item you want to get ",gt= 0)):
     return biedronka[item_id]
 
-@app.get("/get-by-name")
-def store(name : str):
+@app.get("/get-by-name/{item_id}")
+def get_by_name(item_id : int, name : Optional[str]= None ):
     for item_id in biedronka:
         if biedronka[item_id]['product'] == name:
             return biedronka[item_id]
     return {'Data' : 'Not Found'}
+
+
+@app.post("/create-item/{item_id}")
+def create_item(item_id : int, item : Item):
+    if item_id in biedronka:
+        return {"Error" : "Item is already exist in Biedronka"}
+
+    biedronka[item_id] = {'product': item.product, 'price' : item.price, 'brand':item.brand, 'quantity': item.quantity}
+    return biedronka[item_id]
 
 
 
